38380a9ec08b8bf5c72c4336187ef979fd37714c
stivensson <stivensson.is@gmail.com>
2023-07-29 18:43:37 +0300

    task

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,18 +1,20 @@
 
+import React, { Component } from 'react'
 import './Task.css'
 
-const Task = ( {description, created} ) => {
-    return (
-        <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>
-                <span className="description">{description}</span>
-                <span className="created">{created}</span>
-            </label>
-            <button className="icon icon-edit"></button>
-            <button className="icon icon-destroy"></button>
-        </div>
-    )
+export default class Task extends Component {
+    render() {
+        const {description, created} = this.props
+        return (
+            <div className="view">
+                <input className="toggle" type="checkbox" />
+                <label>
+                    <span className="description">{description}</span>
+                    <span className="created">{created}</span>
+                </label>
+                <button className="icon icon-edit"></button>
+                <button className="icon icon-destroy"></button>
+            </div>
+        )
+    }
 }
-
-export default Task
